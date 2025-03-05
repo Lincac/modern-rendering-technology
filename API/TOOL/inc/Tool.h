79b96d353a65834930a9b2f822023b5dd780a143
Lincac <83110022+Lincac@users.noreply.github.com>
2025-03-05 10:57:28 +0800

    feat：测试加载模型接口

---
+++ API/TOOL/inc/Tool.h
@@ -0,0 +1,101 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <fstream>
+
+#include <glm.hpp>
+
+/** @Lincian
+ * 目前该接口只能接受 顶点属性 v 和 面属性 f 的模型文件
+ */
+template<typename T>
+void objread(const std::string& path, std::vector<T>& vertex, std::vector<int>& face)
+{
+}
+
+template<>
+void objread(const std::string& path, std::vector<float>& vertex, std::vector<int>& face)
+{
+	std::ifstream file(path);
+	std::string line;
+
+	if (file.is_open())
+	{
+		while (std::getline(file, line))
+		{
+			std::istringstream iss(line);
+
+			char type;
+			iss >> type;
+
+			if (type == 'v')
+			{
+				float v1, v2, v3;
+
+				iss >> v1 >> v2 >> v3;
+
+				vertex.push_back(v1);
+				vertex.push_back(v2);
+				vertex.push_back(v3);
+			}
+			else if (type == 'f')
+			{
+				int f1, f2, f3;
+
+				iss >> f1 >> f2 >> f3;
+
+				face.push_back(f1);
+				face.push_back(f2);
+				face.push_back(f3);
+			}
+		}
+	}
+	else
+	{
+		std::cout << "Error : can not read path : " << path << std::endl;
+	}
+}
+
+template<>
+void objread(const std::string& path, std::vector<glm::vec3>& vertex, std::vector<int>& face)
+{
+	std::ifstream file(path);
+	std::string line;
+
+	if (file.is_open())
+	{
+		while (std::getline(file, line))
+		{
+			std::istringstream iss(line);
+
+			char type;
+			iss >> type;
+
+			if (type == 'v')
+			{
+				float v1, v2, v3;
+
+				iss >> v1 >> v2 >> v3;
+
+				vertex.push_back({ v1, v2, v3 });
+			}
+			else if (type == 'f')
+			{
+				int f1, f2, f3;
+
+				iss >> f1 >> f2 >> f3;
+
+				face.push_back(f1);
+				face.push_back(f2);
+				face.push_back(f3);
+			}
+		}
+	}
+	else
+	{
+		std::cout << "Error : can not read path : " << path << std::endl;
+	}
+}
--- OIT/main.cpp
+++ OIT/main.cpp
@@ -1,6 +1,26 @@
+/** @Lincian
+ * 该OIT方式是使用深度剥离算法实现
+ */
+
 #include <iostream>
+
+#include <glad.h>
+#include <glfw3.h>
+#include <glm.hpp>
+#include <gtc/matrix_transform.hpp>
+#include <gtc/quaternion.hpp>
+#define GLM_ENABLE_EXPERIMENTAL
+#include <gtx/quaternion.hpp>
+#include <ShaderProgram.h>
+#include <Camera.h>
+#include <Tool.h>
+
+Camera camera;
 
 int main()
 {
-    std::cout << "hello" << std::endl;
+	std::vector<glm::vec3> vertex;
+	std::vector<int> face;
+
+	objread("../../models/cube.obj", vertex, face);
 }
